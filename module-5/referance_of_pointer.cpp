271d414fcda4812b19f7aa13c3bee8d20c4d9f52
programmer-nur <pesfootball83@gmail.com>
2025-12-22 20:30:35 +0600

    added referance of pointer

---
+++ module-5/referance_of_pointer.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fun(int *&p) { *p = 100; }
+
+int main() {
+  int x = 10;
+
+  int *p = &x;
+
+  fun(p);
+
+  cout << x << endl;
+  return 0;
+}

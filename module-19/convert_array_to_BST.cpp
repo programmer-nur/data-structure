3791cd4e52c497a5c80e2df1d5cffdd191334586
programmer-nur <pesfootball83@gmail.com>
2026-02-04 14:20:27 +0600

    convert arry to bst

---
+++ module-19/convert_array_to_BST.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+  int val;
+  Node *left;
+  Node *right;
+
+  Node(int val) {
+    this->val = val;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+
+void level_order(Node *root) {
+  queue<Node *> q;
+
+  if (root)
+    q.push(root);
+
+  while (!q.empty()) {
+    Node *p = q.front();
+    q.pop();
+
+    cout << p->val << " ";
+
+    if (p->left)
+      q.push(p->left);
+    if (p->right)
+      q.push(p->right);
+  }
+}
+
+Node *convert(int a[], int n, int l, int r) {
+
+  if (l > r)
+    return NULL;
+  int mid = (l + r) / 2;
+  Node *root = new Node(a[mid]);
+
+  Node *leftRoot = convert(a, n, l, mid - 1);
+  Node *rightRoot = convert(a, n, mid + 1, r);
+
+  root->left = leftRoot;
+  root->right = rightRoot;
+  return root;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int a[n];
+
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  Node *root = convert(a, n, 0, n - 1);
+  level_order(root);
+
+  return 0;
+}
---
+++ module-19/insert_in_BST.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+  int val;
+  Node *left;
+  Node *right;
+
+  Node(int val) {
+    this->val = val;
+    this->left = NULL;
+    this->right = NULL;
+  }
+};
+
+Node *inputTree() {
+  int val;
+  cin >> val;
+  Node *root;
+  if (val == -1)
+    root = NULL;
+  else
+    root = new Node(val);
+
+  queue<Node *> q;
+  if (root)
+    q.push(root);
+
+  while (!q.empty()) {
+    Node *p = q.front();
+    q.pop();
+
+    int l, r;
+    cin >> l >> r;
+
+    Node *myLeft, *myRight;
+
+    if (l == -1)
+      myLeft = NULL;
+    else
+      myLeft = new Node(l);
+    if (r == -1)
+      myRight = NULL;
+    else
+      myRight = new Node(r);
+
+    p->left = myLeft;
+    p->right = myRight;
+
+    if (p->left)
+      q.push(p->left);
+    if (p->right)
+      q.push(p->right);
+  }
+
+  return root;
+}
+
+void level_order(Node *root) {
+  queue<Node *> q;
+
+  if (root)
+    q.push(root);
+
+  while (!q.empty()) {
+    Node *p = q.front();
+    q.pop();
+
+    cout << p->val << " ";
+
+    if (p->left)
+      q.push(p->left);
+    if (p->right)
+      q.push(p->right);
+  }
+}
+
+void insert(Node *root, int val) {
+  if (root == NULL)
+    root = new Node(val);
+
+  queue<Node *> q;
+
+  if (root->val > val) {
+    if (root->left == NULL)
+      root->left = new Node(val);
+    else
+      insert(root->left, val);
+  } else {
+    if (root->right == NULL)
+      root->right = new Node(val);
+    else
+      insert(root->right, val);
+  }
+}
+
+int main() {
+  Node *root = inputTree();
+  int val;
+  cin >> val;
+  insert(root, val);
+  level_order(root);
+  return 0;
+}
